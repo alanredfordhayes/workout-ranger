37880c0ca780c62c305703f970ddcfd34abcfc14
Alan Hayes <ahayes@dotdash.com>
2022-09-28 09:06:35 +0200

    app 2 load

---
+++ app_2/app_2.py
@@ -0,0 +1,62 @@
+import json
+import urllib3
+import boto3
+import base64
+from botocore.exceptions import ClientError
+
+def scan_products_db():
+    client = boto3.client('dynamodb')
+    try: response = client.scan(TableName='TableName1')
+    except: response = 'Could not scan items'
+    return response
+
+def get_secret():
+    secret_name = "placid"
+    region_name = "us-east-1"
+    session = boto3.session.Session()
+    client = session.client(service_name='secretsmanager', region_name=region_name)
+    try: get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException': raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException': raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException': raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException': raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException': raise e
+    else:
+        if 'SecretString' in get_secret_value_response: 
+            text_secret_data = json.loads(get_secret_value_response['SecretString']); return text_secret_data
+        else: decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary']); return decoded_binary_secret
+
+def create_image (image, cta, price, title, token):
+    http = urllib3.PoolManager()
+    encoded_body = json.dumps(
+        {
+            "create_now": True,
+            "layers": {
+                "img": {
+                    "image": image,
+                    "image_viewport": "1200x1200"
+                },
+                "cta": {
+                    "text": cta
+                },
+                "price": {
+                    "text": price
+                },
+                "title": {
+                    "text": title
+                }
+            }
+        }
+    )
+    bearer_token = 'Bearer ' + token
+    r = http.request('POST', 'https://api.placid.app/api/rest/ysr4e4n8l', headers={'Content-Type': 'application/json', 'Authorization': bearer_token }, body=encoded_body)
+    return(r.data)
+
+def lambda_handler(event, context):
+    s = scan_products_db()
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!'),
+        'output' : s
+    }
---
+++ lambda_app_2.tf
@@ -0,0 +1,152 @@
+##LOCALS
+locals {
+  app_2_name = "${var.name}-${random_string.random.result}-social"
+  #LAMBDA
+  ##aws_lambda_function
+  aws_lambda_function_app_2_function_name = "${local.app_2_name}"
+  aws_lambda_function_app_2_description   = "${local.app_2_name}"
+  aws_lambda_function_app_2_timeout       = 3
+  ##aws_lambda_permission
+  aws_lambda_permission_app_2_statement_id  = "AllowExecutionFromAPIGateway"
+  aws_lambda_permission_app_2_action        = "lambda:InvokeFunction"
+  aws_lambda_permission_app_2_principal      = "apigateway.amazonaws.com"
+  #IAM
+  ##aws_iam_role
+  aws_iam_role_app_2_name = "${local.app_2_name}"
+  ##aws_iam_role_policy_attachment
+  aws_iam_role_policy_attachment_app_2_policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  ##aws_iam_policy
+  aws_iam_policy_app_2_name = "${local.app_2_name}"
+  aws_iam_policy_app1_path  = "/"
+  aws_iam_policy_app_2_description = "${local.app_2_name}"
+  #Cloudwatch
+  ##aws_cloudwatch_log
+  aws_cloudwatch_log_app_2_group_name = "${local.app_2_name}"
+}
+
+##LAMBDA
+resource "aws_lambda_function" "app_2" {
+  function_name     = local.aws_lambda_function_app_2_function_name
+  description       = local.aws_lambda_function_app_2_description
+  timeout           = local.aws_lambda_function_app_2_timeout
+  runtime           = "python3.9"
+  handler           = "app_2.lambda_handler"
+  s3_bucket         = aws_s3_bucket.app_2.id
+  s3_key            = aws_s3_object.app_2.key
+  source_code_hash  = data.archive_file.app_2.output_base64sha256
+  role              = aws_iam_role.app_2.arn
+  environment {
+    variables = {
+      TableName1 = local.aws_dynamodb_table_app_1_db_1
+      TableName2 = local.aws_dynamodb_table_app_1_db_2
+      TableName3 = local.aws_dynamodb_table_app_1_db_3
+      TableName4 = local.aws_dynamodb_table_app_1_db_4
+      TableName5 = local.aws_dynamodb_table_app_1_db_5
+    }
+  }
+}
+
+resource "aws_lambda_permission" "app_2" {
+  statement_id  = local.aws_lambda_permission_app_2_statement_id
+  action        = local.aws_lambda_permission_app_2_action
+  function_name = aws_lambda_function.app_2.function_name
+  principal     = local.aws_lambda_permission_app_2_principal
+}
+
+##S3
+data "archive_file" "app_2" { 
+  type        = "zip"
+  source_dir  = "${path.module}/app_2"
+  output_path = "${path.module}/app_2.zip"
+}
+
+resource "aws_s3_bucket" "app_2" {
+  bucket_prefix = local.app_2_name
+  force_destroy = true
+}
+
+resource "aws_s3_object" "app_2" {
+  bucket  = aws_s3_bucket.app_2.id
+  key     = "app_2.zip"
+  source  = data.archive_file.app_2.output_path
+  etag    = filemd5(data.archive_file.app_2.output_path)
+}
+
+##IAM
+resource "aws_iam_role" "app_2" {
+  name = local.aws_iam_role_app_2_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "app_2" {
+  role       = aws_iam_role.app_2.name
+  policy_arn = local.aws_iam_role_policy_attachment_app_2_policy_arn
+}
+
+resource "aws_iam_policy" "app_2" {
+    name = local.aws_iam_policy_app_2_name
+    path = local.aws_iam_policy_app1_path
+    description = local.aws_iam_policy_app_2_description
+    policy = jsonencode(
+        {
+          Version = "2012-10-17"
+          Statement = [
+            {
+              "Sid": "VisualEditor0",
+              "Effect": "Allow",
+              "Action": [
+                  "secretsmanager:GetRandomPassword",
+                  "logs:CreateLogGroup",
+                  "secretsmanager:ListSecrets"              ],
+              "Resource": "*"
+            },
+            {
+              "Sid": "VisualEditor1",
+              "Effect": "Allow",
+              "Action": [
+                  "secretsmanager:GetResourcePolicy",
+                  "secretsmanager:GetSecretValue",
+                  "dynamodb:PutItem",
+                  "secretsmanager:DescribeSecret",
+                  "dynamodb:GetItem",
+                  "dynamodb:UpdateItem",
+                  "dynamodb:UpdateTable",
+                  "secretsmanager:ListSecretVersionIds"
+              ],
+              "Resource": [
+                  "arn:aws:dynamodb:us-east-1:${var.AWS_ACCOUNT_NUMBER}:table/${local.aws_dynamodb_table_app_1_db_1}",
+                  "arn:aws:dynamodb:us-east-1:${var.AWS_ACCOUNT_NUMBER}:table/${local.aws_dynamodb_table_app_1_db_2}",
+                  "arn:aws:dynamodb:us-east-1:${var.AWS_ACCOUNT_NUMBER}:table/${local.aws_dynamodb_table_app_1_db_3}",
+                  "arn:aws:dynamodb:us-east-1:${var.AWS_ACCOUNT_NUMBER}:table/${local.aws_dynamodb_table_app_1_db_4}",
+                  "arn:aws:dynamodb:us-east-1:${var.AWS_ACCOUNT_NUMBER}:table/${local.aws_dynamodb_table_app_1_db_5}",
+                  "arn:aws:secretsmanager:us-east-1:${var.AWS_ACCOUNT_NUMBER}:secret:placid-JmBBUn"
+              ]
+            }
+          ]
+        }
+    )
+}
+
+resource "aws_iam_policy_attachment" "app_2" {
+  name       = aws_iam_policy.app_2.name
+  roles      = [aws_iam_role.app_2.name]
+  policy_arn = aws_iam_policy.app_2.arn
+}
+
+##cloudwatch
+resource "aws_cloudwatch_log_group" "app_2" {
+  name = local.aws_cloudwatch_log_app_2_group_name
+  retention_in_days = 30
+}
