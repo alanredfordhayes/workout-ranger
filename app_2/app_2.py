2429bac917b5aab0c75ca895ea093118d708e936
Alan Hayes <ahayes@dotdash.com>
2022-10-03 15:07:57 -0400

    get image raw

--- app_2/app_2.py
+++ app_2/app_2.py
@@ -21,7 +21,7 @@ def create_social_media_post():
     sm_post = {}
     num = 0
     for item in product_list_items: 
-        response = client.get_item(TableName=os.environ['TableName5'],Key={'id': {'S': item['id']['N'] } } )
+        response = client.get_item(TableName=os.environ['TableName5'],Key={'id': {'N': 29529640075310 } } )
         num = num + 1
         sm_post[num] = {
             'number' : num,
