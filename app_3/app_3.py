cf3bfe2bb149c4d4203a2f6e0686eedd71b3d87f
Alan Hayes <ahayes@dotdash.com>
2022-10-16 11:20:14 -0400

    instagram app

--- app_3/app_3.py
+++ app_3/app_3.py
@@ -1,5 +1,6 @@
 import json
 import urllib3
+from urllib.parse import urlencode
 import boto3
 import base64
 from botocore.exceptions import ClientError
@@ -39,12 +40,23 @@ def processing_messages():
     text_secret_data = get_secret()
     instagram_page_id = text_secret_data['page_id']
     instagram_access_token = text_secret_data['access_token']
-    url = 'https://graph.facebook.com/v15.0/' + instagram_page_id + '?fields=instagram_business_account&access_token=' + instagram_access_token
+    facebook_api = 'https://graph.facebook.com/v15.0/'
+    url = facebook_api + instagram_page_id + '?fields=instagram_business_account&access_token=' + instagram_access_token
     r = http.request('GET', url)
     instagram_ids = json.loads(r.data.decode('utf-8'))
     instagram_business_account_id = instagram_ids['instagram_business_account']['id']
     print(instagram_business_account_id)
-
+    encoded_args = urlencode(
+        {
+            'image_url' : 'https://s3-placid.s3.eu-central-1.amazonaws.com/production/rest-images/b3y0kkhnt/rest-e1ab7132136b4e94d547d64334a7b429-ruglxtfd.jpg',
+            'caption' : 'WorkoutRanger',
+            'access_token' : instagram_access_token
+        }
+    )
+    
+    url = facebook_api + instagram_page_id + 'media?' + encoded_args
+    r = http.request('POST', url )
+    instagram_ids = json.loads(r.data.decode('utf-8'))
     return 
     
 
